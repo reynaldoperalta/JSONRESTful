41d945bea9dacdb1f8e048b6099e7c8b90ea5c3f
reynaldoperalta <reynaldopema14@gmail.com>
2021-11-17 12:22:44 -0800

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by reynaldo peralta marquez on 11/17/21.
+//  Copyright © 2021 reynaldo peralta marquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
